7c4a64fc8bd22823b7904b31f2cb9fb8bc645f44
Ray Agni <jagni001@ucr.edu>
2024-10-12 13:05:48 -0700

    [ray] add requirements.txt and basic flask app setup

--- server/app.py
+++ server/app.py
@@ -2,3 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def showHomePage():
+
+    return """
+        This is a temporary home page, we'll be using this as an api, but for now it is a placeholder
+    """
