b506ce536977678680b9e5f33dab4cbc1d3fd5aa
Jenny Tran <withjennyy@gmail.com>
2024-10-13 04:29:13 -0700

    [Jenny] add time

--- robot/capture.py
+++ robot/capture.py
@@ -2,7 +2,9 @@
 import cv2 as cv
 import os
 import numpy as np
+import time
 
+capture_duration = 10
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
@@ -19,8 +21,8 @@ result = cv.VideoWriter(
     10,
     size
 )
-
-while True:
+start_time = time.time()
+while( int(time.time() - start_time) < capture_duration ):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
