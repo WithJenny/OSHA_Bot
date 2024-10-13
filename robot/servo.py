b84e3de8378384b7c7d902bf49c13d88eac5f6b3
Jenny Tran <withjennyy@gmail.com>
2024-10-13 01:57:12 -0700

    [Jenny] remove throttle

--- robot/servo.py
+++ robot/servo.py
@@ -6,9 +6,9 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 kit.servo[0].angle = 90
-kit.continuous_servo[1].throttle = 1
+
 time.sleep(1)
-kit.continuous_servo[1].throttle = -1
-time.sleep(1)
+
+
 kit.servo[1].angle = 90
-kit.continuous_servo[1].throttle = 0
+time.sleep(1)
